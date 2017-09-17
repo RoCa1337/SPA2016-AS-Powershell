543fc62fa5c97dcd5c9b1d6367be68d2abfd92a5
Administrator@spdom.local <Administrator@spdom.local>
2017-09-17 11:36:57 +0200

    FP 2 Slipstream added

---
+++ 03/PrepareFP2Slipstream.ps1
@@ -0,0 +1,4 @@
+# download sp feature pack 2 - sept 2017 cu
+# extract sharepoint setup iso to d:\spsetup prior to running the line below
+D:\sts2016-kb4011127-fullfile-x64-glb.exe /extract:d:\spetup\updates
+D:\wssloc2016-kb4011112-fullfile-x64-glb.exe /extract:d:\spetup\updates
